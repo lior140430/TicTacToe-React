3b0dbb5ac71e0d14d3aa74387268aff7b609fc76
Rony Eshel <Eshelrony@gmail.com>
2026-02-01 01:59:17 +0200

    feat(adding logic and roles to the game)

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,18 @@
-import { useState } from 'react';
 import styles from './App.module.css';
 import { Board } from './components/board/Board';
-import type { Player } from './components/Square/Square';
-import { Examples } from './Examples';
+import { useTicTacToe } from './hooks/useTicTacToe';
 
 function App() {
-  const [squares, setSquares] = useState<Player[]>(Array(9).fill(null));
-
-  function handlePlay(i: number) {
-    const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
-    setSquares(nextSquares);
-  }
+  const { squares, handlePlay, resetGame, status, winningLine, isDraw } = useTicTacToe();
 
   return (
     <div className={styles.container}>
       <div className={styles.game}>
+        <div className={styles.status}>{status}</div>
         <div className={styles.gameBoard}>
-          <Board squares={squares} onPlay={handlePlay} />
+          <Board squares={squares} onPlay={handlePlay} winningLine={winningLine} isDraw={isDraw} />
         </div>
-      </div>
-
-      <div style={{ marginTop: '50px', borderTop: '1px solid #444', width: '100%' }}>
-        <Examples />
+        <button className={styles.resetButton} onClick={resetGame}>Restart Game</button>
       </div>
     </div>
   )
